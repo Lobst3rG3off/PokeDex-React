6840d9905a36a76d53b51d6420969b87b4fbbf91
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-26 17:09:14 -0400

    the joys of deployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Pagination from './Pagination';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
-  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
   const [nextPageUrl, setNextPageUrl] = useState()
   const [prevPageUrl, setPrevPageUrl] = useState()
 const [ loading, setLoading] = useState(true)
