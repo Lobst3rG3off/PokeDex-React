af1ab240d2f48a9c4d664c9899423dd08368804e
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-26 15:59:40 -0400

    setup for pagination

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
-  const [pokemon, setPokemon] = useState()
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [previousPageUrl, setPreviousPageUrl] = useState()
+
+  
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+      setNextPageUrl(res.data.next)
+      setNextPageUrl(res.data.previous)
+      setPokemon( res.data.results.map( p => p.name))  
+    })
+  }, [currentPageUrl])
+
+
   return (
    <PokemonList pokemon={pokemon} />
   );
